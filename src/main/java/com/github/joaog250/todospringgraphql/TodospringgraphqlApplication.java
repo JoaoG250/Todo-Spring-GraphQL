cd6c456d4c5a938828f19205c2d73266e64d1721
João Rodrigues <joao.devmail@gmail.com>
2022-07-30 15:50:33 -0300

    setup: start spring boot project with gradle

---
+++ src/main/java/com/github/joaog250/todospringgraphql/TodospringgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.joaog250.todospringgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodospringgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodospringgraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/joaog250/todospringgraphql/TodospringgraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joaog250.todospringgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodospringgraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
