a461a722e1fbe5f1effb7da303e93b98e83ee703
João Rodrigues <joao.devmail@gmail.com>
2022-08-05 11:56:32 -0300

    feat: columns manual control and jpa auditing

--- src/main/java/com/github/joaog250/todospringgraphql/model/Todo.java
+++ src/main/java/com/github/joaog250/todospringgraphql/model/Todo.java
@@ -1,23 +1,50 @@
 package com.github.joaog250.todospringgraphql.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "todo")
 public class Todo {
 
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "id", updatable = false, nullable = false)
     private String id;
+    @Column(name = "title", nullable = false, columnDefinition = "TEXT")
     private String title;
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
     private String description;
+    @Column(name = "done", nullable = false)
     private boolean done;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     public Todo() {
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id=" + id + ", title=" + title + ", description=" + description + ", done=" + done
+                + ", createdAt="
+                + createdAt + ", updatedAt=" + updatedAt + "]";
     }
 
     public String getId() {
@@ -50,5 +77,21 @@ public class Todo {
 
     public void setDone(boolean done) {
         this.done = done;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
